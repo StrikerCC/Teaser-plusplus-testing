9e6dceb1083803667f7099ab765eff41a6b983c2
StrikerCC <chengc0611@gmail.com>
2021-10-15 17:30:46 +0800

    dataset for model man point cloud and mr init

---
+++ reg_with_pc_from_mr.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+"""
+@author: Cheng Chen
+@email: chengc0611@gmail.com
+@time: 10/14/21 5:21 PM
+"""
---
+++ remove_extra_mesh.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+"""
+@author: Cheng Chen
+@email: chengc0611@gmail.com
+@time: 10/12/21 4:54 PM
+"""
